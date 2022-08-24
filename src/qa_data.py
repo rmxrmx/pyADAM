61f146f3e1b537b6e0ca5cfea0dec9e8beb1e278
rmxrmx <mrokas@protonmail.com>
2022-08-24 12:09:18 +0200

    Updated QA script

--- src/qa_data.py
+++ src/qa_data.py
@@ -11,6 +11,7 @@ import glob
 # TODO: this will have to be accomodated for the data that Francesca gives us
 
 # method for getting ITI, IOI and asynchronies from onsets
+# Data format: onsets1 = ITI, onsets2= IOI
 def parse_data(onsets1, onsets2):
     iti = np.diff(onsets1)
     ioi = np.diff(onsets2)
@@ -23,23 +24,17 @@ def parse_data(onsets1, onsets2):
     return iti, ioi, asyn
 
 
-# TODO: since missed_taps is optional, two_participants should be coded via TapTimes,
-# where it will be ITI - IOI - Metronome
-# if missed_taps is True, assume that taps with onset = 0 are missed (except the 1st one)
-# TODO: this is not ideal, because now matronome onsets are passed just to say that there are 2
-# participants. Should probably just have a condition that says how many there are
-def qa_data(TapTimes, MissedCrit, TestRange=None, asyn_bound=0.5, missed_taps=False):
+# If missed_taps is True, assume that taps with onset = 0 are missed (except the 1st one)
+# Data should be passed such that The first tap time refers to ITI, second to IOI
+def qa_data(TapTimes, MissedCrit, TestRange=None, asyn_bound=0.5, missed_taps=False, two_participants=False):
 
-    # TODO: need to incorporate missed_taps
+    # clean = whether the data is good enough (below critical number of misses)
+    clean = True
+    interpolated = False
+    n_interpolations = 0
 
-    # if there are two dimensions (i.e. Metronome + ITI), assume a single participant
-    if np.ndim(TapTimes) == 2:
-        two_participants = False
-        TapTimes, TapTimes2 = TapTimes
-    else:
-        # split the pairs into separate variables so the original code works
-        two_participants = True
-        TapTimes, TapTimes2, metronome = TapTimes
+    # split the pairs into separate variables so the original code works
+    TapTimes, TapTimes2,  = TapTimes
 
     # asyn, ITI and IOI should be computed here
     ITI, IOI, asyn = parse_data(TapTimes, TapTimes2)
@@ -50,6 +45,9 @@ def qa_data(TapTimes, MissedCrit, TestRange=None, asyn_bound=0.5, missed_taps=Fa
 
         taps_missed[0] = False
         taps_missed2[0] = False
+    else:
+        taps_missed = [False] * len(TapTimes)
+        taps_missed2 = [False] * len(TapTimes2)
 
     # if TestRange is given, assume there are synchronization+continuation phases
     # otherwise, just a single phase
@@ -60,14 +58,17 @@ def qa_data(TapTimes, MissedCrit, TestRange=None, asyn_bound=0.5, missed_taps=Fa
         end = len(asyn)
 
     iti = ITI
+    ioi = IOI
 
     # check if there are any asynchronies too big
     bound = IOI * asyn_bound
     large_asyn = np.logical_or(np.array(asyn) < -1 * np.array(bound), np.array(asyn) > np.array(bound))
     invalid = np.logical_or(taps_missed[start : end], large_asyn[start : end])
 
-    n_missed_taps = sum(taps_missed[start : end])
-    n_large_asyn = sum(large_asyn[start : end])
+    # N.B.: n_invalid refers to the first participants invalid rows, while n_invalid2, n_missed_taps2 and
+    # n_large_asyn2 refers to BOTH of the participants' invalids. If two_participants = False, then
+    # n_missed_taps2, n_invalid2 and n_large_asyn2 are still used, but they refer to the SINGLE participants
+    # values.
     n_invalid = sum(invalid)
 
     if two_participants:
@@ -75,38 +76,41 @@ def qa_data(TapTimes, MissedCrit, TestRange=None, asyn_bound=0.5, missed_taps=Fa
         large_asyn2 = np.logical_or(np.array(asyn) < -1 * np.array(bound), np.array(asyn) > np.array(bound))
         invalid2 = np.logical_or(taps_missed2[start : end], large_asyn2[start : end])
 
-        n_missed_taps2 = sum(taps_missed2[start : end])
-        n_large_asyn2 = sum(large_asyn2[start : end])
-        n_invalid2 = sum(invalid2)
-    # otherwise set to 0 to allow the next lines to work
+        n_missed_taps2 = sum(np.logical_or(taps_missed[start : end], taps_missed2[start : end]))
+        n_large_asyn2 = sum(np.logical_or(large_asyn[start : end], large_asyn2[start : end]))
+        n_invalid2 = sum(np.logical_or(invalid, invalid2))
     else:
-        n_missed_taps2 = 0
-        n_large_asyn2 = 0
-        n_invalid2 = 0
+        n_missed_taps2 = sum(taps_missed[start : end])
+        n_large_asyn2 = sum(large_asyn[start : end])
+        n_invalid2 = sum(invalid)
 
 
     print("Checking data quality")
-    print("{} large asynchronies within Test Range".format(n_large_asyn + n_large_asyn2))
-    print("{} missed taps within Test Range".format(n_missed_taps + n_missed_taps2))
-    print("{} total invalid rows".format(n_invalid + n_invalid2))
+    print("{} large asynchronies within Test Range".format(n_large_asyn2))
+    print("{} missed taps within Test Range".format(n_missed_taps2))
+    print("{} total invalid rows".format(n_invalid2))
 
 
-    if n_invalid + n_invalid2 > MissedCrit:
+    if n_invalid2 > MissedCrit:
+        clean = False
         print("Data rejected. Reason: too many invalid rows.")
-        # might need to change these to accomodate two participants
-        return TapTimes, ITI, IOI, asyn
+        return (TapTimes, TapTimes2), ITI, IOI, asyn, clean, interpolated, n_missed_taps2, n_large_asyn2, n_interpolations
 
     for i in range(len(invalid) - 1):
         if invalid[i] and invalid[i + 1] or (two_participants and invalid2[i] and invalid2[i + 1]):
+            clean = False
             print("Data rejected. Reason: data has consecutive invalid rows.")
-            return TapTimes, ITI, IOI, asyn
+            return (TapTimes, TapTimes2), ITI, IOI, asyn, clean, interpolated, n_missed_taps2, n_large_asyn2, n_interpolations
     
     if n_invalid > 0:
+        interpolated = True
         print("Interpolating missing data using average")
 
         invalid_indx = [i for i, x in enumerate(invalid) if x]
 
         for index in invalid_indx:
+            n_interpolations += 1
+
             # If element is at the start or end, repeat the next / previous value 
             # ("average" of 1 value)
             if index == 0:
@@ -124,38 +128,42 @@ def qa_data(TapTimes, MissedCrit, TestRange=None, asyn_bound=0.5, missed_taps=Fa
         iti = np.diff(TapTimes)
         iti = np.insert(iti, 0, 0)
 
-    # Note: if the asynchrony is big both ways (when counting from ITI and IOI)
-    # then this will run just to fix an async that has already been fixed.
-    # This could be changed to account for that.
-    if two_participants and n_invalid2 > 0:
-        print("Interpolating missing data using average (2nd participant)")
-
-        invalid_indx = [i for i, x in enumerate(invalid2) if x]
-
-        for index in invalid_indx:
-            # If element is at the start or end, repeat the next / previous value 
-            # ("average" of 1 value)
-            if index == 0:
-                asyn[index + start] = asyn[index + start + 1]
-                TapTimes2[index + start] = TapTimes2[index + start + 1]
-            elif index + start == end - 1:
-                asyn[index + start] = asyn[index + start - 1]
-                TapTimes2[index + start] = TapTimes2[index + start - 1]
-            else:
-                asyn[index + start] = (asyn[index + start - 1] + asyn[index + start + 1]) / 2
-                TapTimes2[index + start] = (TapTimes2[index + start - 1] + TapTimes2[index + start + 1]) / 2
-
-            print("Interpolated trial {} (2nd participant)".format(index + start + 1))
-        
-        ioi = np.diff(TapTimes2)
-        ioi = np.insert(ioi, 0, 0)
-
-
-    # TODO: need to return stuff from MATLAB that's not currently here
+    # N.B.: the bound is rechecked because a large asynchrony may be fixed by the part above
+    # and a row that was tagged as 'invalid' would now be fine.
     if two_participants:
-        return (TapTimes, TapTimes2), iti, ioi, asyn
-    else:
-        return TapTimes, iti, IOI, asyn
+        bound = iti * asyn_bound
+        large_asyn2 = np.logical_or(np.array(asyn) < -1 * np.array(bound), np.array(asyn) > np.array(bound))
+        invalid2 = np.logical_or(taps_missed2[start : end], large_asyn2[start : end])
+
+        n_invalid2 = sum(invalid2)
+
+        if n_invalid2 > 0:
+            interpolated = True
+            print("Interpolating missing data using average (2nd participant)")
+
+            invalid_indx = [i for i, x in enumerate(invalid2) if x]
+
+            for index in invalid_indx:
+                n_interpolations += 1
+
+                # If element is at the start or end, repeat the next / previous value 
+                # ("average" of 1 value)
+                if index == 0:
+                    asyn[index + start] = asyn[index + start + 1]
+                    TapTimes2[index + start] = TapTimes2[index + start + 1]
+                elif index + start == end - 1:
+                    asyn[index + start] = asyn[index + start - 1]
+                    TapTimes2[index + start] = TapTimes2[index + start - 1]
+                else:
+                    asyn[index + start] = (asyn[index + start - 1] + asyn[index + start + 1]) / 2
+                    TapTimes2[index + start] = (TapTimes2[index + start - 1] + TapTimes2[index + start + 1]) / 2
+
+                print("Interpolated trial {} (2nd participant)".format(index + start + 1))
+            
+            ioi = np.diff(TapTimes2)
+            ioi = np.insert(ioi, 0, 0)
+
+    return (TapTimes, TapTimes2), ITI, IOI, asyn, clean, interpolated, n_missed_taps2, n_large_asyn2, n_interpolations
 
 
 
@@ -172,8 +180,7 @@ test_range = [21, 71]
 
 iti, ioi, asyn = parse_data(onsets1, onsets2)
 
-onsets, iti, ioi, asyn = qa_data((onsets1, onsets2), 4, test_range)
-
+onsets, iti, ioi, asyn, clean, interpolated, n_missed_taps, n_large_asyn, n_interpolations = qa_data((onsets1, onsets2), 4, test_range, two_participants=True)
 
 # ignore the first value, since it is calculated with reference to 0
 # TODO: might need to be revisited when we have data
