277223bdce5a40a4c9339c1667feb4f173dbfe2a
rmxrmx <mrokas@protonmail.com>
2023-02-07 17:07:28 +0100

    update recovery analysis

--- src/parameter_recovery/parameter_recovery_analysis.py
+++ src/parameter_recovery/parameter_recovery_analysis.py
@@ -10,44 +10,16 @@ from src.adam_modules.adaptation_model import adaptation_model
 # A results dataframe for storing behavioural data and parameter estimations
 results = pd.DataFrame(
     columns=[
-        "filename",
-        "run",
         "actual_alpha",
         "actual_beta",
         "actual_delta",
         "actual_phi",
-        "leader",
-        "follower",
         "model",
         "alpha",
         "beta",
         "delta",
         "phi",
-        "sT",
-        "sM",
-        "LLE",
-        "median_abs_async",
-        "min_abs_async",
-        "max_abs_async",
-        "mean_async",
         "sd_async",
-        "mean_abs_async",
-        "sd_abs_async",
-        "min_async",
-        "max_async",
-        "median_ITI",
-        "mean_ITI",
-        "sd_ITI",
-        "min_ITI",
-        "max_ITI",
-        "median_IOI",
-        "mean_IOI",
-        "sd_IOI",
-        "min_IOI",
-        "max_IOI",
-        "CV_ITI",
-        "CV_async",
-        "n_events",
     ]
 )
 
@@ -71,29 +43,8 @@ for file in files:
         asyn = data.iloc[:, 1].tolist()[4:]
 
         # Behavioural data
-        median_abs_asyn = np.median(np.abs(asyn)).round(3)
-        min_abs_asyn = np.min(np.abs(asyn))
-        max_abs_asyn = np.max(np.abs(asyn))
-        mean_asyn = np.mean(asyn).round(3)
         # N.B.: SD is different from MATLAB - divides by N instead of N-1
         sd_asyn = np.std(asyn).round(3)
-        mean_abs_asyn = np.mean(np.abs(asyn)).round(3)
-        sd_abs_asyn = np.std(np.abs(asyn)).round(3)
-        min_asyn = np.min(asyn)
-        max_asyn = np.max(asyn)
-        median_iti = np.median(iti)
-        mean_iti = np.mean(iti).round(3)
-        sd_iti = np.std(iti).round(3)
-        min_iti = np.min(iti)
-        max_iti = np.max(iti)
-        median_ioi = np.median(ioi)
-        mean_ioi = np.mean(ioi).round(3)
-        sd_ioi = np.std(ioi).round(3)
-        min_ioi = np.min(ioi)
-        max_ioi = np.max(ioi)
-        cv_iti = (sd_iti / mean_iti).round(3)
-        cv_asyn = (sd_asyn / mean_iti).round(3)
-        n_events = len(ioi)
 
         # Split by _ to get the actual values of the run.
         # Note: this part of the script assumes the datafiles are named
@@ -104,7 +55,6 @@ for file in files:
         actual_beta = params[5]
         actual_delta = params[6]
         actual_phi = params[7]
-        run_no = params[8][3:]
         model = params[9][:-4]
 
         # calculate the parameters
@@ -125,44 +75,16 @@ for file in files:
             beta = 0
 
         results.loc[len(results.index)] = [
-            file,
-            run_no,
             actual_alpha,
             actual_beta,
             actual_delta,
             actual_phi,
-            leader,
-            follower,
             model,
             alpha,
             beta,
             mE,
             gamma,
-            sT,
-            sM,
-            LL,
-            median_abs_asyn,
-            min_abs_asyn,
-            max_abs_asyn,
-            mean_asyn,
             sd_asyn,
-            mean_abs_asyn,
-            sd_abs_asyn,
-            min_asyn,
-            max_asyn,
-            median_iti,
-            mean_iti,
-            sd_iti,
-            min_iti,
-            max_iti,
-            median_ioi,
-            mean_ioi,
-            sd_ioi,
-            min_ioi,
-            max_ioi,
-            cv_iti,
-            cv_asyn,
-            n_events,
         ]
 
 results.to_csv("parameter_recovery_results.csv", index=None)
